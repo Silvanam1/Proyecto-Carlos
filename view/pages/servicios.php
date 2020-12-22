30ce4afb62089f5082a214cb2be0d5690bd1be27
Silvana <silvanabmaldonado@hotmail.com>
2020-12-21 23:39:15 -0300

    Comienzo del NAV

---
+++ view/pages/servicios.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../general.css">
+    <title>Servicios</title>
+</head>
+<body>
+    <?php
+    require('../shell/header.php');
+    ?>
+    <a href="../../index.php">ir al index</a>
+    <p>este es el contenido de la pagina servicios</p>
+    <?php
+    require('../shell/footer.php');
+    ?>
+</body>
+</html>
--- view/shell/header.php
+++ view/shell/header.php
@@ -1,3 +1,9 @@
 <header>
-    <h1>esta es la pagina web de carlos</h1>
+    <h1> DIMVER </h1>
+    <nav> 
+        <a href="http://localhost/Proyecto-Carlos/index.php"> Index </a>
+        <a href="http://localhost/Proyecto-Carlos/view/pages/nosotros.php"> Nosotros </a>
+        <a href="http://localhost/Proyecto-Carlos/view/pages/servicios.php"> Servicios </a>
+        <a href="http://localhost/Proyecto-Carlos/view/pages/contacto.php"> Contacto </a>
+    </nav>
 </header>
