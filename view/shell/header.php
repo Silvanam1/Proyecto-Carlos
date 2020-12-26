d3faec51ab2aca3ea4fe6ad98f3e3973d0e96c1f
Rodrigo <rcosentino@candoit.com.ar>
2020-12-26 16:19:10 -0300

    estilos header

--- view/shell/header.php
+++ view/shell/header.php
@@ -1,7 +1,7 @@
-<header>
-    <h1> DIMVER </h1>
+<header id="header">
+    <h1><a href="http://localhost/Proyecto-Carlos/index.php"> DIMVER </a></h1>
     <nav> 
-        <a href="http://localhost/Proyecto-Carlos/index.php"> Index </a>
+        <!--<a href="http://localhost/Proyecto-Carlos/index.php"> Index </a>-->
         <a href="http://localhost/Proyecto-Carlos/view/pages/nosotros.php"> Nosotros </a>
         <a href="http://localhost/Proyecto-Carlos/view/pages/servicios.php"> Servicios </a>
         <a href="http://localhost/Proyecto-Carlos/view/pages/cartelera.php"> Cartelera </a>
