554c95629e8ba380c3934f56aaf4ae3bc052e610
Rodrigo <rcosentino@candoit.com.ar>
2021-01-03 12:33:12 -0300

    servicios falta aprobación

--- view/pages/servicios.php
+++ view/pages/servicios.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="http://localhost/Proyecto-Carlos/general.css">
+    <link rel="stylesheet" href="../../general.css">
+    <link rel="stylesheet" href="servicios.css">
     <title>Servicios</title>
 </head>
 <body>
@@ -23,18 +24,25 @@
                     <p class="texto"> 
                         <ul class="lista">
                             <li>
-                                <p class="texto">
-                                <span class="negrita">ASESORAMIENTO TÉCNICO EN SISTEMAS DE SEGURIDAD ELECTRÓNICA Y CÁMARAS DE SEGURIDAD:</span> 
+                            
+                            <button class="collapsible"><span class="negrita">ASESORAMIENTO TÉCNICO EN SISTEMAS DE SEGURIDAD ELECTRÓNICA Y CÁMARAS DE SEGURIDAD</span></button>
+                            <div class="content">
+                                <p class="texto"> 
                                 Realizando un relevamiento teniendo en cuenta según lo que se evalué mediante un análisis de riesgo 
                                 del lugar a instalar, para empresas, edificios, sindicatos, municipalidades, Agro, concesionaria de 
                                 autos y/o Camiones, como así los distintos rubros que sean solicitados para tal fin, con abono mensual 
                                 a pactar según la necesidad de cada caso en particular. Realizando una propuesta para cada caso 
                                 puntual. -  
                                 </p>
+                            </div>
+
                             </li>
                             <li>
+                            
+                            <button class="collapsible"><span class="negrita">ALARMAS MONITOREADAS</span></button>
+                            <div class="content">
                                 <p class="texto">
-                                <span class="negrita">ALARMAS MONITOREADAS:</span> Enlazado con la Central de Monitoreo 24/7, con distintos medios 
+                                Enlazado con la Central de Monitoreo 24/7, con distintos medios 
                                 de comunicación para la transferencia de información generada por el sistema de seguridad 
                                 electrónica. -
                                 </p>
@@ -46,17 +54,27 @@
                                 de la empresa, también a los operadores que se encuentren de turno, quedando registrado en el reporte 
                                 el sabotaje o hackeo informático. – 
                                 </p>
+                            </div>
+
                             </li>
 
                             <li>
+
+                            <button class="collapsible"><span class="negrita">SEGURIDAD PERIMETRAL</span></button>
+                            <div class="content">
                                 <p class="texto">
-                                <span class="negrita">SEGURIDAD PERIMETRAL:</span>  Instalación y análisis para domicilios, edificios, terrenos 
+                                Instalación y análisis para domicilios, edificios, terrenos 
                                 (Alerta temprana ante una usurpación de su propiedad)    
                                 </p>
+                            </div>
+                            
                             </li>
                             <li>
+
+                            <button class="collapsible"><span class="negrita">PULSADORES DE PÁNICO</span></button>
+                            <div class="content">
                                 <p class="texto">
-                                <span class="negrita">PULSADORES DE PÁNICO:</span>  Para Víctimas de violencia de género, en el caso de ser abonado 
+                                <span class="negrita">Para Víctimas de violencia de género</span>, en el caso de ser abonado 
                                 y se tenga una perimetral vigente, y se viole la misma, con el requisito acreditar mediante oficio 
                                 judicial y firmado por el magistrado que interviniente, y notificación de la dependencia policial 
                                 que tomo intervención. Una vez cumplido con los requisitos nuestra empresa DIMVER (SIN COSTO ALGUNO),
@@ -69,72 +87,119 @@
                                 (Este ultimo se posee un costo de abono mensual). –
                                 </p>
                                 <p class="texto">
-                                <span class="negrita">Pulsadores de pánico:</span>  Para personas de avanzada edad, nuestra empresa Dimver (SIN COSTO),
+                                <span class="negrita">Pulsadores de pánico para personas de avanzada edad</span>, nuestra empresa Dimver (SIN COSTO),
                                 se realizara la instalación en el lugar y equipamiento en (comodato), con el fin de resguardar la 
                                 integridad física de la persona teniendo en cuenta, que en el lugar se instalara un pulsador con 
                                 tres botones, que incluirá la comunicación de forma directa con el operador de turno, tendrá  médico,
                                 bomberos  y Policía, de forma inmediata al lugar en el caso que se necesario y fuera evaluado por el
                                 operador de turno de esta empresa. Mediante comunicación con el 911.   
                                 </p>
+                            </div>
+                            
                             </li>
                             <li>
+
+                            <button class="collapsible"><span class="negrita">CÁMARAS DE SEGURIDAD</span></button>
+                            <div class="content">
                                 <p class="texto">
-                                <span class="negrita">CÁMARAS DE SEGURIDAD:</span>  Instalación de cámaras Análogas, IP , Inteligencia Artificial (A.I).-
+                                  Instalación de cámaras Análogas, IP , Inteligencia Artificial (A.I).-
                                 </p>
+                            </div>
+                            
                             </li>
                             <li>
+
+                            <button class="collapsible"><span class="negrita">PORTERÍA VIRTUAL</span></button>
+                            <div class="content">
                                 <p class="texto">
-                                <span class="negrita">PORTERÍA VIRTUAL:</span>  Consta de un operador por turno, la instalación de cámaras de 
+                                  Consta de un operador por turno, la instalación de cámaras de 
                                 seguridad en el lugar, se tiene una comunicación inmediata con cada uno de los propietarios. 
                                 Listado de personas Autorizadas y Ocasionales al ingreso, donde se encuentra instalado dicho sistema. - 
                                 </p>
+                            </div>
+                            
                             </li>
                             <li>
+
+                            <button class="collapsible"><span class="negrita">CONTROL DE ACCESO Y PRESENTISMO</span></button>
+                            <div class="content">
                                 <p class="texto">
-                                <span class="negrita">CONTROL DE ACCESO Y PRESENTISMO:</span>  Para empresas, Depósitos o se adapte a la necesidad 
+                                  Para empresas, Depósitos o se adapte a la necesidad 
                                 del futuro potencial abonado.  
                                 </p>
+                            </div>
+                            
                             </li>
-                                <li>
+                            <li>
+
+                            <button class="collapsible"><span class="negrita">MONITOREO DE CAJAS FUERTES</span></button>
+                            <div class="content">
                                 <p class="texto">
-                                <span class="negrita">MONITOREO DE CAJAS FUERTES:</span>  Instalación y supervisión mediante un sistema de alarmas 
+                                  Instalación y supervisión mediante un sistema de alarmas 
                                 monitoreado en tiempo real, como así también instalación detectores sísmicos. 
                                 </p>
+                            </div>
+                            
                             </li>
                             <li>
+
+                            <button class="collapsible"><span class="negrita">SISTEMAS DE INCENDIO</span></button>
+                            <div class="content">
                                 <p class="texto">
-                                    <span class="negrita">SISTEMAS DE INCENDIO:</span>   Instalación y detección, redes, Extinción de incendios.  
+                                       Instalación y detección, redes, Extinción de incendios.  
                                 </p> 
+                            </div>
+                            
                             </li>
                             <li>
+
+                            <button class="collapsible"><span class="negrita">ASISTENCIA TELEFÓNICA</span></button>
+                            <div class="content">
                                 <p class="texto"> 
-                                <span class="negrita">ASISTENCIA TELEFÓNICA:</span>  Las 24/7 con una cantidad de operadores de turno, ante cualquier 
+                                  Las 24/7 con una cantidad de operadores de turno, ante cualquier 
                                 duda o consulta, que podrán resolver o enviar el soporte del personal idóneo para solucionar la falla 
                                 técnica si lo hubiere.
                                 </p>
+                            </div>
+                            
                             </li>
                             <li>
+
+                            <button class="collapsible"><span class="negrita">SISTEMAS DE SEGURIDAD PERSONALIZADOS</span></button>
+                            <div class="content">
                                 <p class="texto">
-                                <span class="negrita">SISTEMAS DE SEGURIDAD PERSONALIZADOS:</span>  Teniendo en cuanta la protección de la persona 
+                                  Teniendo en cuanta la protección de la persona 
                                 resguardando la integridad física de las personas y bienes materiales. En cualquier ámbito laboral, 
                                 como en lo personal, pudiendo adaptar el sistema de seguridad electrónica quedando óptimas condiciones 
                                 de uso, teniendo en cuenta, como así quedando sujeto al análisis de riesgo y relevamiento del lugar.
                                 </p>
+                            </div>
+                            
                             </li>
                             <li>
+
+                            <button class="collapsible"><span class="negrita">INFORMES</span></button>
+                            <div class="content">
                                 <p class="texto">
-                                <span class="negrita">INFORMES:</span>  De aperturas y cierres, comunicación de forma instantánea de apertura no 
+                                  De aperturas y cierres, comunicación de forma instantánea de apertura no 
                                 autorizadas o fuera del horario que se desempeña de forma habitual, en un domicilio residencial, 
                                 de fin de semana, empresa, deposito y/o oficina.
                                 </p>
+                            </div>
+                            
                             </li>
                             <li>
+
+                            <button class="collapsible"><span class="negrita">SISTEMA DE CONTROL DE RONDAS</span></button>
+                            <div class="content">
                                 <p class="texto">
-                                <span class="negrita">SISTEMA DE CONTROL DE RONDAS:</span> Herramienta esencial complementando la función del personal 
+                                 Herramienta esencial complementando la función del personal 
                                 de seguridad física para una correcta recorrida, generando un informe que puede ser diario, semanal, 
                                 mensual, como así la explicación del sistema de control de rondas y su dispositivo para marcar y 
                                 registrar la recorrida. 
                                 </p>
+                            </div>
+                            
                             </li>
                         </ul>
 
@@ -145,6 +210,25 @@
 
         </section>
         <script src="https://kit.fontawesome.com/f3c3af2199.js" crossorigin="anonymous"></script>
+        <script>
+            var coll = document.getElementsByClassName("collapsible");
+            var i;
+
+            for (i = 0; i < coll.length; i++) {
+                collapsible(coll);
+            }
+            function collapsible(coll) {
+                coll[i].addEventListener("click", function() {
+                    this.classList.toggle("active");
+                    var content = this.nextElementSibling;
+                    if (content.style.maxHeight){
+                    content.style.maxHeight = null;
+                    } else {
+                    content.style.maxHeight = content.scrollHeight + "px";
+                    } 
+                });
+            }
+        </script>
     <?php
     require('../shell/footer.php');
     ?>
