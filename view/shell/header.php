53bb99d89342b154c4174830508f1cd1bcdccd9d
cosentinowevdev <rodrigocosentino@outlook.com>
2021-08-16 14:00:43 -0300

    cambio logo

--- view/shell/header.php
+++ view/shell/header.php
@@ -2,9 +2,8 @@
     <h1>
         <a href="http://dimver.com.ar/"> 
             <picture>
-                <source srcset="http://dimver.com.ar/img/imagenes/logo2021.png" media="(max-width: 600px)">
-                <source srcset="http://dimver.com.ar/img/imagenes/isologo2021.png" media="(max-width: 1160px)">
-                <img src="http://dimver.com.ar/img/imagenes/isologotipo2021.png">
+                <source srcset="https://dimver.com.ar/img/imagenes/isologo2021.png" media="(max-width: 1160px)">
+                <img src="https://dimver.com.ar/img/imagenes/isologotipo2021.png">
             </picture>
         </a>
     </h1>
