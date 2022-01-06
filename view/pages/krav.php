8f746fc8443346d4ff4910ab1dabe35097c4afa2
cosentinowevdev <rodrigocosentino@outlook.com>
2022-01-06 02:46:48 -0300

    add dropdown menu video

--- view/pages/krav.php
+++ view/pages/krav.php
@@ -20,7 +20,7 @@
 
 <body>
     <?php
-    require('../shell/header.php');
+    require('../shell/headerKrav.php');
     ?>
     <section class="card" id="Servicios">
         <div>
@@ -38,9 +38,10 @@
 
                         <h3 class="negrita centrado">Emrich Lichtenfeld</h3>
                         <figure class="centrado">
-                            <img src="../../img/kravmaga/kravMaga1.jpeg" alt="tips de krav maga imagen " class="img imgGrandes">
+                            <img id="myImg" src="../../img/kravmaga/kravMaga1.jpeg" alt="tips de krav maga imagen " onclick="imagenModal(this)" class="img imgGrandes">
                         </figure>
                     </article>
+
 
                 </section>
 
@@ -58,7 +59,7 @@
                                 certificados y diplomas carecen de validez.
                             </p>
                             <p class="texto">
-                                Fue conformada por un grupo de instructores capacitados en las
+                            Fue conformada por un grupo de instructores capacitados en las
                                 mas prestigiosas asociaciones de Israel y formados por grandes y
                                 destacados Maestros Israelíes, pero con una mirada acorde a la
                                 realidad legal y económica como la de nuestro país.
@@ -70,7 +71,7 @@
                                 <article class="" style="background-color: white; justify-content:center;display:grid">
                                     <h3 class="negrita centrado">World Krav Maga Group</h3>
                                     <figure class="centrado">
-                                        <img src="../../img/kravmaga/kravMaga3.jpeg" alt="krav maga logo" class="img imagenMuyGrande">
+                                        <img id="myImg" src="../../img/kravmaga/kravMaga3.jpeg" alt="krav maga logo" onclick="imagenModal(this)" class="img imagenMuyGrande">
                                     </figure>
                                 </article>
                             </section>
@@ -132,7 +133,7 @@
                             <section class="texto">
                                 <article class="" style="background-color: white; justify-content:center;display:grid">
                                     <figure class="centrado">
-                                        <img src="../../img/kravmaga/kravMaga2.jpeg" alt="krav maga tips" class="img imagenMuyGrande">
+                                        <img id="myImg" src="../../img/kravmaga/kravMaga2.jpg" alt="krav maga tips" onclick="imagenModal(this)" class="img imagenMuyGrande">
                                     </figure>
                                 </article>
                             </section>
@@ -140,7 +141,38 @@
                         </div>
 
                     </li>
+                    <li>
 
+                        <button class="collapsible"><span class="negrita">VENTAJAS DE PERSONAS QUE SON CAPACITADAS CON ESTE SISTEMA DE DEFENSA</span></button>
+                        <div class="content">
+                            <p class="texto">
+                            <ul>
+                                <li>
+                                    <br>
+                                    <h3>ASPECTOS PSICOLÓGICOS: </h3>
+                                    <br>
+                                    <ul style="list-style: disc;     margin-left: 20px;">
+                                        <li class="lista">Aumenta la confianza y seguridad de cada persona capacitada en sí mimo, el grado agresividad correctamente para salir de la situación de peligro.</li>
+                                        <li class="lista">Despierta el interés y el afán de superación, enseña estrategias de autocontrol, Incrementa el nivel de autoestima, permitiéndole perder el miedo al contacto físico.</li>
+                                        <li class="lista">Aumenta la iniciativa y mejora el grado de concentración, incorpora hábitos saludables a sus rutinas diarias y así favorece la socialización.</li>
+                                    </ul>
+                                    <br>
+                                    <br>
+                                </li>
+                                <li>
+                                    <h3>ASPECTOS FÍSICOS: </h3>
+                                    <br>
+                                    <ul style="list-style: disc;     margin-left: 20px;">
+                                        <li class="lista">Mejora notablemente la psicomotricidad, como así el equilibrio, tanto estático como dinámico.</li>
+                                        <li class="lista">Aumenta la fuerza, potencia, flexibilidad y la elasticidad.</li>
+                                        <br>
+                                    </ul>
+                                </li>
+                            </ul>
+                            </p>
+                        </div>
+
+                    </li>
 
                     <li>
 
@@ -148,14 +180,15 @@
                         <div class="content">
                             <p class="texto">
                                 Nuestros cursos de autoprotección, desarrollados en conjunto con la empresa Dimver, se adaptan a
-                                las necesidades especificas de cada colectivo social o profesional
+                                las necesidades especificas de cada colectivo social o profesional con el fin de resguardar la 
+                                integridad física del personal que fue capacitado
                             </p>
                             <section class="texto">
                                 <article class="" style="background-color: white; justify-content:center;display:grid">
 
                                     <h3 class="negrita centrado">Armas de entrenamiento</h3>
                                     <figure class="centrado">
-                                        <img src="../../img/kravmaga/kravMaga6.jpeg" alt="Normas IRAM" class="img imagenMuyGrande">
+                                        <img id="myImg" src="../../img/kravmaga/kravMaga6.jpeg" alt="Normas IRAM" onclick="imagenModal(this)" class="img imagenMuyGrande">
                                     </figure>
                                 </article>
                             </section>
@@ -168,32 +201,32 @@
                             <section class="texto">
                                 <article class="" style="background-color: white; justify-content:center;display:grid">
                                     <figure class="centrado">
-                                        <img src="../../img/kravmaga/tipsKravMagaDimver1.jpg" alt="tips de krav maga imagen 1" class="img imagenMuyGrande">
+                                        <img id="myImg" src="../../img/kravmaga/tipsKravMagaDimver1.jpg" alt="tips de krav maga imagen 1" onclick="imagenModal(this)" class="img imagenMuyGrande">
                                     </figure>
                                 </article>
                                 <article class="" style="background-color: white; justify-content:center;display:grid">
                                     <figure class="centrado">
-                                        <img src="../../img/kravmaga/tipsKravMagaDimver2.jpg" alt="tips de krav maga imagen 2" class="img imagenMuyGrande">
+                                        <img id="myImg" src="../../img/kravmaga/tipsKravMagaDimver2.jpg" alt="tips de krav maga imagen 2" onclick="imagenModal(this)" class="img imagenMuyGrande">
                                     </figure>
                                 </article>
                                 <article class="" style="background-color: white; justify-content:center;display:grid">
                                     <figure class="centrado">
-                                        <img src="../../img/kravmaga/tipsKravMagaDimver3.jpg" alt="tips de krav maga imagen 3" class="img imagenMuyGrande">
+                                        <img id="myImg" src="../../img/kravmaga/tipsKravMagaDimver3.jpg" alt="tips de krav maga imagen 3" onclick="imagenModal(this)" class="img imagenMuyGrande">
                                     </figure>
                                 </article>
                                 <article class="" style="background-color: white; justify-content:center;display:grid">
                                     <figure class="centrado">
-                                        <img src="../../img/kravmaga/tipsKravMagaDimver4.jpg" alt="tips de krav maga imagen 4" class="img imagenMuyGrande">
+                                        <img id="myImg" src="../../img/kravmaga/tipsKravMagaDimver4.jpg" alt="tips de krav maga imagen 4" onclick="imagenModal(this)" class="img imagenMuyGrande">
                                     </figure>
                                 </article>
                                 <article class="" style="background-color: white; justify-content:center;display:grid">
                                     <figure class="centrado">
-                                        <img src="../../img/kravmaga/tipsKravMagaDimver5.jpg" alt="tips de krav maga imagen 5" class="img imagenMuyGrande">
+                                        <img id="myImg" src="../../img/kravmaga/tipsKravMagaDimver5.jpg" alt="tips de krav maga imagen 5" onclick="imagenModal(this)" class="img imagenMuyGrande">
                                     </figure>
                                 </article>
                                 <article class="" style="background-color: white; justify-content:center;display:grid">
                                     <figure class="centrado">
-                                        <img src="../../img/kravmaga/tipsKravMagaDimver6.jpg" alt="tips de krav maga imagen 6" class="img imagenMuyGrande">
+                                        <img id="myImg" src="../../img/kravmaga/tipsKravMagaDimver6.jpg" alt="tips de krav maga imagen 6" onclick="imagenModal(this)" class="img imagenMuyGrande">
                                     </figure>
                                 </article>
                             </section>
@@ -204,8 +237,56 @@
 
                 </ul>
                 <article class="" style="background-color: white; justify-content:center;display:grid">
+                    <!-- <iframe width="560" height="315" src="https://www.youtube.com/embed/anrREpMv5eY?rel=0?showinfo=0?controls=2?modestbranding=0?autoplay=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> -->
+                    <div id="player"></div>
+
+                    <script>
+                        // 2. This code loads the IFrame Player API code asynchronously.
+                        var tag = document.createElement('script');
+
+                        tag.src = "https://www.youtube.com/iframe_api";
+                        var firstScriptTag = document.getElementsByTagName('script')[0];
+                        firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+                        // 3. This function creates an <iframe> (and YouTube player)
+                        //    after the API code downloads.
+                        var player;
+                        function onYouTubeIframeAPIReady() {
+                            player = new YT.Player('player', {
+                            height: '360',
+                            width: '640',
+                            videoId: 'anrREpMv5eY',
+                            playerVars: { 'autoplay': 1, 'controls': 0 , 'showinfo':0},
+                            events: {
+                                'onReady': onPlayerReady,
+                                'onStateChange': onPlayerStateChange
+                            }
+                            });
+                        }
+
+                        // 4. The API will call this function when the video player is ready.
+                        function onPlayerReady(event) {
+                            event.target.playVideo();
+                        }
+
+                        // 5. The API calls this function when the player's state changes.
+                        //    The function indicates that when playing a video (state=1),
+                        //    the player should play for six seconds and then stop.
+                        var done = false;
+                        function onPlayerStateChange(event) {
+                            if (event.data == YT.PlayerState.PLAYING && !done) {
+                            setTimeout(stopVideo, 60);
+                            done = true;
+                            }
+                        }
+                        function stopVideo() {
+                            player.stopVideo();
+                        }
+                    </script>
+                </article>
+                <article class="" style="background-color: white; justify-content:center;display:grid">
                     <figure class="centrado">
-                        <img src="../../img/kravmaga/kravMaga4.jpeg" alt="asociacion con dimver" class="img imagenMuyGrande">
+                        <img id="myImg" src="../../img/kravmaga/kravMaga4.jpg" alt="asociacion con dimver" onclick="imagenModal(this)" class="img imagenMuyGrande">
                     </figure>
                 </article>
 
@@ -214,10 +295,21 @@
         </div>
 
     </section>
+    <div id="myModal" class="modal">
+
+        <!-- The Close Button -->
+        <span class="close">&times;</span>
+
+        <!-- Modal Content (The Image) -->
+        <img class="modal-content" id="img01">
+
+        <!-- Modal Caption (Image Text) -->
+        <!--<div id="caption"></div> -->
+    </div>
     <link rel="stylesheet" href="../../general.css">
     <script src="https://kit.fontawesome.com/f3c3af2199.js" crossorigin="anonymous"></script>
     <script src="https://dimver.com.ar/controller/servicios.js"></script>
-
+    <script src="../../controller/modalImg.js"></script>
     <?php
     require('../shell/footer.php');
     ?>
